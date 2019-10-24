8f4e69da55b12b38b8c7958ffe485ed74cf90915
Fu Helen <helenxfubiz@gmail.com>
2019-10-24 12:08:20 +0900

    testing setup jest cypress

---
+++ tests/e2e/specs/cypress.spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+// command to open cypress
+// node_modules/.bin/cypress open
+
+describe('test', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('testing if it works', () => { })
+})
---
+++ tests/unit/specs/jest.spec.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+
+//TODO need setup to work
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
